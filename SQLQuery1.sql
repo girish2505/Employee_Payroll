fbd6e5f7ad5d7777d603dd5af0455b2abbd4866b
Girish Guptha Vayugundla <girish.v@tvsnext.io>
2021-08-05 16:09:05 +0530

    Inserting data into the employee payroll table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,6 +3,7 @@
 create database payroll_service;
 
 use payroll_service;
+
 ---------UC2-------------
 create table employee_payroll
 (
@@ -12,3 +13,7 @@ salary float,
 startDate date,
 emailId varchar(20)
 );
+
+----------UC3-------------
+Insert into employee_payroll(name,salary,startDate,emailId) values ('girish',25000,'2021-05-14','v.g@gmail.com'),('madhu',28000,'2021-07-21','v.m@gmail.com');
+Insert into employee_payroll values ('nivas',90000,'2016-03-16','v.n@gmail.com'),('pavan',23000,'2020-11-30','pavan@gmail.com');
