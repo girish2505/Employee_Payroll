6cdbd67d7138c5c63baf3ddab78cbfb71403d410
Girish Guptha Vayugundla <girish.v@tvsnext.io>
2021-08-05 15:57:54 +0530

    Creating the employee payroll table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,3 +3,12 @@
 create database payroll_service;
 
 use payroll_service;
+---------UC2-------------
+create table employee_payroll
+(
+empId int identity(1,1) primary key,
+name varchar(20) not null,
+salary float,
+startDate date,
+emailId varchar(20)
+);
