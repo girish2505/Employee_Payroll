2b4b7e3c954d446aeb1402d8e7386389a874bf65
Girish Guptha Vayugundla <girish.v@tvsnext.io>
2021-08-05 16:14:34 +0530

    retrive all the data from employee payroll table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -17,3 +17,6 @@ emailId varchar(20)
 ----------UC3-------------
 Insert into employee_payroll(name,salary,startDate,emailId) values ('girish',25000,'2021-05-14','v.g@gmail.com'),('madhu',28000,'2021-07-21','v.m@gmail.com');
 Insert into employee_payroll values ('nivas',90000,'2016-03-16','v.n@gmail.com'),('pavan',23000,'2020-11-30','pavan@gmail.com');
+
+----------UC4--------------
+select * from employee_payroll;
